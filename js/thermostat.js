f191897590ecf8271eca917502c25ac7077b17dc
Emily Sas <emily.bronwen.sas@gmail.com>
2015-01-21 14:39:25 +0000

    removing ability to change defaults - creating methods inside Thermostat constructor

--- js/thermostat.js
+++ js/thermostat.js
@@ -1,27 +1,37 @@
 var Thermostat = function() {
   this.powerSavingMode = true;
   this.temperature = 20;
-  this.minTemperature = 10;
-  this.maxTemperature = 32;
+  var defaultMin = 10;
+  var defaultMax = 32;
   this.resetTo = 20;
   this.maxOnPowerSave = 25;
   this.greenTemperature = 18;
   this.redTemperature = 24;
+  // this.minTemperature = defaultMin;
+  // this.maxTemperature = defaultMax;
+
+  this.getDefaultMin = function() {
+    return defaultMin;
+  };
+
+  this.getDefaultMax = function() {
+    return defaultMax;
+  };
 
 };
 
 Thermostat.prototype.increaseTemperature = function(changeTempBy) {
   var newTemp = this.temperature += changeTempBy;
   if (this.powerSavingMode) {
-    this.temperature = Math.min(newTemp, this.maxOnPowerSave)
+    this.temperature = Math.min(newTemp, this.maxOnPowerSave);
   } else {
-    this.temperature = Math.min(newTemp, this.maxTemperature);
+    this.temperature = Math.min(newTemp, this.getDefaultMax());
   };
 };
 
 Thermostat.prototype.decreaseTemperature = function(changeTempBy) {
   var newTemp = this.temperature -= changeTempBy;
-  this.temperature = Math.max(newTemp, this.minTemperature)
+  this.temperature = Math.max(newTemp, this.getDefaultMin());
   return this.temperature;
 };
 
@@ -52,6 +62,22 @@ Thermostat.prototype.tempReset = function() {
   this.temperature = this.resetTo
 };
 
-Thermostat.prototype.changeMinTemp = function(temp) {
-  this.minTemperature = temp
+// Thermostat.prototype.changeMinTemp = function(temp) {
+//   this.minTemperature = temp
+// };
+
+// Thermostat.prototype.changeMaxTemp = function(temp) {
+//   this.maxTemperature = temp
+// };
+
+Thermostat.prototype.changeDefaults = function(defaultToChange, change) {
+  this[defaultToChange] = change;
 };
+
+
+
+
+
+
+
+
--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -83,23 +83,21 @@ describe("Thermostat", function() {
 
   });
 
-  describe("changing defaults", function() {
+  // describe("changing defaults", function() {
     
-    it("will allow to change default minimum temp", function() {
-      thermostat.changeMinTemp(15);
-      expect(thermostat.minTemperature).toEqual(15);
-    });
+  //   it("will allow user to change default minimum temp", function() {
+  //     thermostat.changeDefaults("minTemperature", 15);
+  //     expect(thermostat.minTemperature).toEqual(15);
+  //   });
+
+  //   it("will allow user to change default maximum temp", function() {
+  //     thermostat.changeDefaults("maxTemperature", 40);
+  //     expect(thermostat.maxTemperature).toEqual(40);
+  //   });
     
-  });
+  // });
+
+
+
 
 });
-
-// Thermostat starts at 20 degrees
-// You can increase the temp with the up button
-// You can decrease the temp with the down button
-// The minimum temperature is 10 degrees
-// If power saving mode is on, the maximum temperature is 25 degrees
-// If power saving mode is off, the maximum temperature is 32 degrees
-// Power saving mode is on by default
-// You can reset the temperature to 20 by hitting the reset button
-// thermostat should color the display based on energy usage - < 18 is green, < 25 is yellow, otherwise red
