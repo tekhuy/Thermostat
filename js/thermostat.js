6135435f41ff4b22d9745fcf4cf185c80bb2be01
Emily Sas <emily.bronwen.sas@gmail.com>
2015-01-21 11:42:43 +0000

    refactoring out magic numbers

--- js/thermostat.js
+++ js/thermostat.js
@@ -1,22 +1,25 @@
 var Thermostat = function() {
   this.temperature = 20;
   this.minimumTemperature = 10;
+  this.maximumTemperature = 32;
+  this.maxOnPS = 25;
   this.powerSavingMode = true;
+  this.resetTo = 20
 };
 
 Thermostat.prototype.increaseTemperature = function(changeTempBy) {
   var newTemp = this.temperature += changeTempBy;
   if (this.powerSavingMode === true){
-    this.temperature = Math.min(newTemp, 25)
+    this.temperature = Math.min(newTemp, this.maxOnPS)
   } else {
-    this.temperature = Math.min(newTemp, 32)
+    this.temperature = Math.min(newTemp, this.maximumTemperature)
   };
   return this.temperature;
 };
 
 Thermostat.prototype.decreaseTemperature = function(changeTempBy) {
   var newTemp = this.temperature -= changeTempBy;
-  this.temperature = Math.max(newTemp, 10)
+  this.temperature = Math.max(newTemp, this.minimumTemperature)
   return this.temperature;
 };
 
@@ -25,9 +28,10 @@ Thermostat.prototype.powerSavingModeSwitch = function() {
     this.powerSavingMode = false;
   } else {
     this.powerSavingMode = true;
+    this.temperature = Math.max(this.temperature, this.maxOnPS)
   };
 };
 
 Thermostat.prototype.tempReset = function() {
-  this.temperature = 20;
+  this.temperature = this.resetTo
 };
